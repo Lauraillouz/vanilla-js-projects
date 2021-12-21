4dd250a40ea7c9e078fad47215ab6016e67d0884
laura.illouz <laura.illouz@raja.com>
2021-12-21 11:14:35 +0100

    sidebar ok

---
+++ sidebar/app.js
@@ -0,0 +1,14 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", () => {
+  // This is how toggle a class works. Redoing it as a reminder
+  /* if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+  } else {
+    sidebar.classList.add("show-sidebar");
+  } */
+
+  sidebar.classList.toggle("show-sidebar");
+});
